eb9787e2e4818aac1cc1d25ae7c0c0a794a149e8
june <conrickjd2@gmail.com>
2025-08-12 10:31:18 -0700

    fix contact + pages.yml

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/website/",  // must match repo name for GitHub Pages
+  base: isProduction ? '/hhwebsite/' : '/website/',// must match repo name for GitHub Pages
   plugins: [react()],
   server: {
     host: '0.0.0.0',
